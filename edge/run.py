7af8c2499c9c3bcdb328c2dc09a7b75a6edd19be
arlingan1 <ashwinreddy51@gmail.com>
2025-08-29 16:28:04 -0400

    command and control changes

--- edge/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Entry point for GRM Service (Async Edge Core Implementation)
-"""
-import asyncio
-from grm_service.main_async import main
-
-if __name__ == "__main__":
-    asyncio.run(main())
